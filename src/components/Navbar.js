2f52106ba6a0effac40cf75a0182ba36fd212ebe
LucasTavares12 <tavareslucas1213@gmail.com>
2026-02-04 11:19:40 -0300

    Atualização para o mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../imagens/logoMarca.png";
+import { FaBars } from "react-icons/fa";
 
 function Navbar() 
 {
   const [scrolled, setScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -29,16 +31,19 @@ function Navbar()
     <div className="nav-right">
 
     {/* BOTÃO 3 PONTINHOS (APENAS MOBILE VIA CSS) */}
-      <div className="menu-toggle" id="menuToggle">
-        ⋮
-      </div>
+        <div
+          className="menu-toggle"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <FaBars />
 
-      <ul className="nav-links">
-        <li><a href="#home">Início</a></li>
-        <li><a href="#beneficios">Vantagens</a></li>
-        <li><a href="#instalacao-garantia">Serviços</a></li>
-        <li><a href="#contato">Contato</a></li>
-      </ul>
+        </div>
+          <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+            <li><a href="#home" onClick={() => setMenuOpen(false)}>Início</a></li>
+            <li><a href="#beneficios" onClick={() => setMenuOpen(false)}>Vantagens</a></li>
+            <li><a href="#instalacao-garantia" onClick={() => setMenuOpen(false)}>Serviços</a></li>
+            <li><a href="#contato" onClick={() => setMenuOpen(false)}>Contato</a></li>
+          </ul>
         <a
           href="https://wa.me/5598991101999"
           target="_blank"
